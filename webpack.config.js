665753861cb57f6be1d0117d70be2c17f381894d
Jeremy Allard <jeremy@allard.io>
2019-03-12 03:10:39 -0400

    Integrate project build toolchain with webpack.

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+
+module.exports = [
+  {
+    name: 'main',
+    entry: './src/main.ts',
+    target: 'electron-main',
+    mode: "production",
+    node: { __dirname: false, __filename: true},
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  },
+  {
+    name: 'renderer',
+    entry: './src/renderer.tsx',
+    target: 'electron-renderer',
+    mode: "production",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          include: path.resolve(__dirname, 'src'),
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+      filename: 'renderer.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  }]
