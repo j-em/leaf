9f9095002154148ac2749eb224895555cd4f7f53
Jeremy Allard <jeremy@allard.io>
2019-04-22 22:29:36 -0400

    Update useRect implementation.

--- src/useRect.ts
+++ src/useRect.ts
@@ -1,14 +1,17 @@
-import { useRef, useState, useEffect } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 
-const useRect = () => {
-  const ref = useRef<Element>();
-  const [dimensions, setDimensions] = useState<
-    import("./useSelection").Rect
-  >();
-  useEffect(() => {
-    ref.current && setDimensions(ref.current.getBoundingClientRect() as DOMRect);
-  }, [ref.current]);
-  return { ref, dimensions };
+const useRect = <T extends HTMLElement>(
+  ref: React.MutableRefObject<T | undefined>,
+  deps: any[] = []
+): DOMRect | undefined => {
+  const [state, setState] = useState<DOMRect>();
+  useLayoutEffect(() => {
+    if (ref.current) {
+      const rect = ref.current.getBoundingClientRect() as DOMRect;
+      setState(rect);
+    }
+  }, deps);
+  return state;
 };
 
 export default useRect;
