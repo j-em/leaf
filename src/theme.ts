731626447027ddd21660c1a270e4bc6f32fd6efa
Jeremy Allard <jeremy@allard.io>
2019-04-22 22:37:33 -0400

    Update application theme.
    Add reference to predefined Carbon theme in app theme module.

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,14 @@
 import "@ibm/plex/css/ibm-plex.css";
-import { colors } from "@carbon/colors"
+import { colors } from "@carbon/colors";
+
+import * as _Theme from "@carbon/themes";
+type ThemePackage = typeof _Theme.default;
+
+const Theme = (_Theme as unknown) as ThemePackage;
+
 export default {
   colors,
+  roles: Theme.themes.g10,
   fonts: {
     sans: "IBM Plex Sans"
   }
